ffa26c505b7d072ce561d9b5661ccde72a453ddd
MarharytaKrasikava <marharytakrasikava@gmail.com>
2022-07-04 13:57:51 +0300

    changed upload url and cors config

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,7 @@
-
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://km4vxlfv5g.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://2if6rbfe5c.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
