8ae3aa71159a016705be0a1a1b71cc5031a128fa
MarharytaKrasikava <marharytakrasikava@gmail.com>
2022-08-01 01:22:38 +0300

    cart url updated

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://9ajn09tuli.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://2if6rbfe5c.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  cart: 'http://marharytakrasikava-cart-api-dev.eu-west-1.elasticbeanstalk.com'
 };
 
 export default API_PATHS;
